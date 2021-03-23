b9c73a2d6eaa54ff68e5147927f1c5fd9bfee161
Nick <CoffeeBeforeArch@gmail.com>
2021-03-23 00:33:17 -0500

    add vector example

---
+++ src/08_vectors.cpp
@@ -0,0 +1,23 @@
+// Tutorial example 8
+// A simple example of vectors vectors
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  // Create a vector of integers
+  std::vector<int> v;
+
+  // Fill it with some values
+  for (int i = 0; i < 10; i++) {
+    // Adds the value of "i" to the vector
+    // The vector will grow if necessary
+    v.push_back(i);
+  }
+
+  // We can use range-based for loops with our STL containers
+  for (int i : v) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+}
